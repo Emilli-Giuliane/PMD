c46607034fec01af62873ed612f3fc368456dacd
Emilli-Giuliane <emilligiuliane@proton.me>
2026-04-21 20:42:01 -0300

    lógica para validar a vitória semi-pronta

--- script.js
+++ script.js
@@ -8,16 +8,18 @@ document.querySelector('#startGame').addEventListener('pointerup', () => {
 })
 
 function gameStart() {
-
+    
     let segurando = [];
+    let frascoOrigem = null;
+    let contadorDeFrascosTerminados = 0;
     
     document.querySelectorAll(".frasco").forEach(frasco => {
-        
+
         frasco.addEventListener('pointerdown', (e) => {
             let bolas = Array.from(frasco.querySelectorAll(".ball"));
             let bola = [];
             let corBola = [];
-
+            
             bolas.forEach(b => {
                 
                 if (b.classList.contains("ballY")) {
@@ -33,19 +35,21 @@ function gameStart() {
                     bola.push(b);
                     corBola.push("Red");
                 }
-
             });
-
+            
+            
+            
             if (segurando.length === 0) {
                 let primeiraBola = bola[0];
                 let verificaCorBola = corBola[0];
-
+                
                 if (primeiraBola != undefined) {
-
+                    // Segurar as bolas
                     for (let i = 0; i < corBola.length; i++){
-
+                        
                         if (verificaCorBola === corBola[i]) {
                             segurando.push(bola[i]);
+                            frascoOrigem = frasco;
                             bola[i].style.transform = 'translate(0px, -4px)';
                             bola[i].style.transition = '.2s';                            
                         } else {
@@ -54,21 +58,68 @@ function gameStart() {
                     };
                 };
             } else {
-                segurando.forEach(bola => {
-
-                    frasco.prepend(bola);
+                function soltarBolas(segurando,frasco) {
+                        segurando.forEach(bola => {
+                            frasco.prepend(bola);                        
+                            bola.style.transform = 'translate(0px, 0px)';
+                            bola.style.transition = '.2s';    
+                        });
+                }
+                
+                // OBS.: Fazer a validação sempre primeiro, e depois fazer as regras
+                // Soltar a bola no mesmo frasco
+                if (frasco === frascoOrigem) {
                     
-                    bola.style.transform = 'translate(0px, 0px)';
-                    bola.style.transition = '.2s';
-                    segurando = []
-                });
-            }
+                    soltarBolas(segurando,frasco);
+                    segurando = [];
+                } else { 
+                    // Soltar se não tiver nenhuma bola dentro
+                    if(bolas.length === 0) { 
+                    
+                        soltarBolas(segurando,frasco);
+                        segurando = [];
+                    
+                    } else  {
+                        // Variáveis de verificação de cores das primeiras bolas sendo seguradas e a primeira bola da fileira de frasco
+                        let segurandoCor = [...segurando[0].classList].find(c => c !== "ball");
+                        let primeiraBolaCor = [...bola[0].classList].find(c => c !== "ball");
+                        
+                        // Soltar a bola em outro frasco
+                        if (bolas.length + segurando.length <= 4 && segurandoCor === primeiraBolaCor) {
+                            
+                            soltarBolas(segurando,frasco);
+                            segurando = [];
+
+                            // Se as cores que eu tenho no frasco + as cores que eu tenho na mão for igual 4, o frasco fica completo
+                            let bolaAtualizada = Array.from(frasco.querySelectorAll(".ball"));
+                            
+                            if (bolaAtualizada.length === 0) return false;
+                            
+                            let corBase = [...bolaAtualizada[0].classList].find(c => c !== "ball");
+
+                            let todasCoresIguais = bolaAtualizada.every(b =>
+                                [...b.classList].find(c => c !== "ball") === corBase
+                            );
+                            
+                            if (todasCoresIguais && bolaAtualizada.length === 4){
+                                contadorDeFrascosTerminados++;
+                                alert(contadorDeFrascosTerminados)
+                            }
+                    
+                        };
+                    
+                    };
+                }; 
+            };
+            
+            
         });
     });
-
+    
 }
 
 gameStart();
+// TODO Amigo: Se caso no futuro ficar dificil de usar o gameStart() por causa da troca de telas, tente reiniciar o layout da tela2 antes de ir para a tela3. Validando a tela3 vá para a ultima tela de vitória.
 
 document.querySelectorAll(".tela").forEach(tela => {
     // Aqui o layout será guardado dentro do DOM de cada .tela
